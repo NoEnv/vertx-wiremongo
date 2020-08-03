b40444cee250d9744992aa319199bdc84620c9ae
Mohammad Naghavi <mohammad@noenv.com>
2020-08-03 16:21:38 +0200

    additional tests for rest of verification's basic functionality

--- src/test/java/com/noenv/wiremongo/verification/VerificationTest.java
+++ src/test/java/com/noenv/wiremongo/verification/VerificationTest.java
@@ -13,6 +13,10 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
+import org.junit.runners.model.MultipleFailureException;
+
+import static org.hamcrest.CoreMatchers.anyOf;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 @RunWith(VertxUnitRunner.class)
 public class VerificationTest extends TestBase {
@@ -20,17 +24,34 @@ public class VerificationTest extends TestBase {
   private Verifier verifier;
 
   @Rule
-  public ExpectedException thrown= ExpectedException.none();
+  public ExpectedException thrown = ExpectedException.none();
 
   @Before
-  public void setUpTest(TestContext ctx) {
+  public void setUpTest() {
     mock.clear();
     verifier = new Verifier();
   }
 
   @After
-  public void tearDownTest(TestContext ctx) {
+  public void tearDownTest() {
     verifier.assertAllSucceeded();
+  }
+
+  @Test
+  public void test_shall_fail_ifDefinedVerificationIsNotRun() {
+
+    thrown.expect(AssertionError.class);
+    thrown.expectMessage("expected 'find one and update in some-collection' to be checked, but it was not");
+
+    mock
+      .findOneAndUpdate()
+      .inCollection("some-collection")
+      .verify(
+        verifier
+          .checkIf("find one and update in some-collection")
+          .isRunExactlyOnce()
+      )
+      .returns(null);
   }
 
   @Test
@@ -59,7 +80,7 @@ public class VerificationTest extends TestBase {
   public void verify_RunExactlyOnce_shall_fail_ifRunTwice(TestContext ctx) {
 
     thrown.expect(AssertionError.class);
-    thrown.expectMessage("expected 'find one and update in some-collection' to run exactly once but ran more often");
+    thrown.expectMessage("expected 'find one and update in some-collection' to run exactly once, but it ran more often");
 
     mock
       .findOneAndUpdate()
@@ -143,6 +164,92 @@ public class VerificationTest extends TestBase {
       .subscribe(CompletableHelper.toObserver(ctx.asyncAssertSuccess()));
   }
 
-  // TODO: 03.08.20 add tests for order checks
+  @Test
+  public void verifyOrder_shall_succeed_ifRunInExpectedOrder(TestContext ctx) {
+    mock
+      .updateCollection()
+      .inCollection("collection-1")
+      .verify(
+        verifier
+          .checkIf("update in collection-1")
+          .isRunAfterPreviousVerifications()
+      )
+      .returns(null);
+
+    mock
+      .updateCollection()
+      .inCollection("collection-2")
+      .verify(
+        verifier
+          .checkIf("update in collection-2")
+          .isRunAfterPreviousVerifications()
+      )
+      .returns(null);
+
+    Completable
+      .concatArray(
+        db
+          .rxUpdateCollection(
+            "collection-1",
+            new JsonObject(),
+            new JsonObject()
+          )
+          .ignoreElement(),
+        db
+          .rxUpdateCollection(
+            "collection-2",
+            new JsonObject(),
+            new JsonObject()
+          )
+          .ignoreElement()
+      )
+      .subscribe(CompletableHelper.toObserver(ctx.asyncAssertSuccess()));
+  }
+
+  @Test
+  public void verifyOrder_shall_fail_ifNotRunInExpectedOrder(TestContext ctx) {
+
+    thrown.expect(MultipleFailureException.class);
+    thrown.expectMessage("There were 2 errors:\n  java.lang.AssertionError(expected 'update in collection-1' to be run before 'update in collection-2', but it was not)\n  java.lang.AssertionError(expected 'update in collection-1' to be checked, but it was not)");
+
+    mock
+      .updateCollection()
+      .inCollection("collection-1")
+      .verify(
+        verifier
+          .checkIf("update in collection-1")
+          .isRunAfterPreviousVerifications()
+      )
+      .returns(null);
+
+    mock
+      .updateCollection()
+      .inCollection("collection-2")
+      .verify(
+        verifier
+          .checkIf("update in collection-2")
+          .isRunAfterPreviousVerifications()
+      )
+      .returns(null);
+
+    Completable
+      .concatArray(
+        db
+          .rxUpdateCollection(
+            "collection-2",
+            new JsonObject(),
+            new JsonObject()
+          )
+          .ignoreElement(),
+        db
+          .rxUpdateCollection(
+            "collection-1",
+            new JsonObject(),
+            new JsonObject()
+          )
+          .ignoreElement()
+      )
+      .subscribe(CompletableHelper.toObserver(ctx.asyncAssertSuccess()));
+  }
 
 }

c4e29e6fb7576fcec6ad9c86872e13c16c038461
Josef Pfleger <green-carrot@orange-cactus.org>
2020-11-13 12:31:40 +0100

    remove vertx workarounds
    Signed-off-by: Josef Pfleger <green-carrot@orange-cactus.org>

--- src/main/java/com/noenv/wiremongo/matching/EqualsMatcher.java
+++ src/main/java/com/noenv/wiremongo/matching/EqualsMatcher.java
@@ -1,8 +1,5 @@
 package com.noenv.wiremongo.matching;
 
-import io.vertx.ext.mongo.BulkWriteOptions;
-import io.vertx.ext.mongo.IndexOptions;
-
 import java.util.Objects;
 
 public class EqualsMatcher<T> implements Matcher<T> {
@@ -15,17 +12,6 @@ public class EqualsMatcher<T> implements Matcher<T> {
 
   @Override
   public boolean matches(T other) {
-
-    //TODO: remove with vertx 4.0 (https://github.com/vert-x3/vertx-mongo-client/pull/168)
-    if (other instanceof IndexOptions && value instanceof IndexOptions) {
-      return Objects.equals(((IndexOptions)value).toJson(), ((IndexOptions)other).toJson());
-    }
-
-    //TODO: remove with vertx 4.0 (https://github.com/vert-x3/vertx-mongo-client/pull/169)
-    if (other instanceof BulkWriteOptions && value instanceof BulkWriteOptions) {
-      return Objects.equals(((BulkWriteOptions)value).toJson(), ((BulkWriteOptions)other).toJson());
-    }
-
     return Objects.equals(value, other);
   }
 

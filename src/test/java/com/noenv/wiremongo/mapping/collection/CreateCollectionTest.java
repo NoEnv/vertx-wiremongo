d210af194c8c4034ca66ae2745cf6529e1ab1c73
Christoph Spörk <christoph.spoerk@gmail.com>
2021-12-22 14:49:18 +0100

    Fix(4.2.3): Add tests for CreateCollectionWithOptions

--- src/test/java/com/noenv/wiremongo/mapping/collection/CreateCollectionTest.java
+++ src/test/java/com/noenv/wiremongo/mapping/collection/CreateCollectionTest.java
@@ -1,6 +1,9 @@
 package com.noenv.wiremongo.mapping.collection;
 
 import com.noenv.wiremongo.TestBase;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.mongo.CollationOptions;
+import io.vertx.ext.mongo.CreateCollectionOptions;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
@@ -23,6 +26,21 @@ public class CreateCollectionTest extends TestBase {
   }
 
   @Test
+  public void testCreateCollectionWithOptions(TestContext ctx) {
+    Async async = ctx.async();
+
+    mock.createCollectionWithOptions()
+      .inCollection("createcollectionwithoptions")
+      .withOptions(new CreateCollectionOptions().setCollation(new CollationOptions().setAlternate(null)))
+      .returns(null);
+
+    db.rxCreateCollectionWithOptions("createcollectionwithoptions",
+      new CreateCollectionOptions().setCollation(new CollationOptions().setAlternate(null))
+    )
+    .subscribe(async::complete, ctx::fail);
+  }
+
+  @Test
   public void testCreateCollectionFile(TestContext ctx) {
     Async async = ctx.async();
     db.rxCreateCollection("createcollectionfile")
@@ -30,12 +48,33 @@ public class CreateCollectionTest extends TestBase {
   }
 
   @Test
+  public void testCreateCollectionWithOptionsFile(TestContext ctx) {
+    Async async = ctx.async();
+    db.rxCreateCollectionWithOptions("createcollectionwithoptionsfile",
+      new CreateCollectionOptions().setCollation(new CollationOptions().setAlternate(null))
+    )
+    .subscribe(async::complete, ctx::fail);
+  }
+
+  @Test
   public void testCreateCollectionFileError(TestContext ctx) {
     Async async = ctx.async();
     db.rxCreateCollection("createcollectionfileerror")
-      .subscribe(() -> ctx.fail(), ex -> {
+      .subscribe(ctx::fail, ex -> {
         ctx.assertEquals("intentional", ex.getMessage());
         async.complete();
       });
+  }
+
+  @Test
+  public void testCreateCollectionWithOptionsFileError(TestContext ctx) {
+    Async async = ctx.async();
+    db.rxCreateCollectionWithOptions("createcollectionwithoptionfileserror",
+        new CreateCollectionOptions().setCollation(new CollationOptions().setAlternate(null))
+    )
+    .subscribe(ctx::fail, ex -> {
+      ctx.assertEquals("intentional", ex.getMessage());
+      async.complete();
+    });
   }
 }
